99b3a4b1228a8d9292bb1a06c33167e673d0db23
Chris Roos <chris@seagul.co.uk>
2018-05-04 16:00:36 +0100

    Return ordered array of answers/percentages

--- server.rb
+++ server.rb
@@ -12,7 +12,7 @@ RESPONSES_VS_COUNTS = {
 get '/' do
   answer = params[:answer]
   unless RESPONSES_VS_COUNTS.keys.include?(answer)
-    { speech: "Please try again" }.to_json
+    { totals: "Please try again" }.to_json
   else
     redis = Redis.new(url: ENV["REDIS_URL"])
     stored_responses_vs_counts = redis.get('responses_vs_counts')
@@ -24,10 +24,17 @@ get '/' do
     responses_vs_counts[answer] += 1
     redis.set "responses_vs_counts", responses_vs_counts.to_json
 
-    speech = responses_vs_counts.sort_by { |response, count| -count }.map do |response, count|
-      "#{count} people chose #{response}"
-    end.join('. ')
+    total_count = responses_vs_counts.inject(0) { |counter, (key, value)| counter + value }
+    totals = responses_vs_counts.sort_by do |key, count|
+      -count
+    end.map do |key, count|
+      percentage = ((count/total_count.to_f)*100).round(2) rescue 0
+      {
+        name: key,
+        percentage: percentage
+      }
+    end
 
-    { speech: speech }.to_json
+    { totals: totals }.to_json
   end
 end
