d801538f1ab03a5d562ec042ee04a750637bb25a
Chris Roos <chris@seagul.co.uk>
2018-05-04 15:18:02 +0100

    Store results in Redis

--- server.rb
+++ server.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
+require 'redis'
+
+RESPONSES_VS_COUNTS = {
+  'biscuit' => 0,
+  'unicorn' => 0,
+  'ice_cream' => 0,
+  'moon_cycle' => 0,
+  'flowerpot' => 0
+}
 
 get '/' do
-  response = {
-    totals: "Response from server"
-  }
-  response.to_json
+  answer = params[:answer]
+  unless RESPONSES_VS_COUNTS.keys.include?(answer)
+    {totals: "Please try again"}.to_json
+  else
+
+    redis = Redis.new(url: ENV["REDIS_URL"])
+    stored_responses_vs_counts = redis.get('responses_vs_counts')
+    responses_vs_counts = if stored_responses_vs_counts
+      JSON.parse(stored_responses_vs_counts)
+    else
+      RESPONSES_VS_COUNTS
+    end
+    responses_vs_counts[answer] += 1
+    redis.set "responses_vs_counts", responses_vs_counts.to_json
+
+    response = {
+      totals: responses_vs_counts
+    }
+    response.to_json
+  end
 end
