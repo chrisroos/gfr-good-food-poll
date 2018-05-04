b5aa548c954def80fde59482c8b09446240a8fa1
Chris Roos <chris@seagul.co.uk>
2018-05-04 15:31:13 +0100

    Respond with friendly text

--- server.rb
+++ server.rb
@@ -12,9 +12,8 @@ RESPONSES_VS_COUNTS = {
 get '/' do
   answer = params[:answer]
   unless RESPONSES_VS_COUNTS.keys.include?(answer)
-    {totals: "Please try again"}.to_json
+    { speech: "Please try again" }.to_json
   else
-
     redis = Redis.new(url: ENV["REDIS_URL"])
     stored_responses_vs_counts = redis.get('responses_vs_counts')
     responses_vs_counts = if stored_responses_vs_counts
@@ -25,9 +24,10 @@ get '/' do
     responses_vs_counts[answer] += 1
     redis.set "responses_vs_counts", responses_vs_counts.to_json
 
-    response = {
-      totals: responses_vs_counts
-    }
-    response.to_json
+    speech = responses_vs_counts.sort_by { |response, count| -count }.map do |response, count|
+      "#{count} people chose #{response}"
+    end.join('. ')
+
+    { speech: speech }.to_json
   end
 end
